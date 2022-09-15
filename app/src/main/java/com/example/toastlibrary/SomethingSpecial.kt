03229f4a1a9cb7747effda9f2ef8e953bec9efbe
VinayakBector2002 <bector.vinayak02@gmail.com>
2022-09-15 14:29:31 -0400

    commit

--- app/src/main/java/com/example/toastlibrary/SomethingSpecial.kt
+++ app/src/main/java/com/example/toastlibrary/SomethingSpecial.kt
@@ -8,7 +8,7 @@ object SomethingSpecial {
     fun show(C : Context, M : String){
         Toast.makeText(C,M,Toast.LENGTH_LONG).show()
     }
-    fun Debugger (context : Context, message : String, tag : String){
+    fun debugger (context : Context, message : String, tag : String){
         show(context,message)
         Log.d(tag,message)
     }
