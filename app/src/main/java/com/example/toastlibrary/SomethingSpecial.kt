c167c5633014521f0263dc0cf00266f826d99f5f
VinayakBector2002 <bector.vinayak02@gmail.com>
2022-09-15 13:57:32 -0400

    Debugger Created

--- app/src/main/java/com/example/toastlibrary/SomethingSpecial.kt
+++ app/src/main/java/com/example/toastlibrary/SomethingSpecial.kt
@@ -1,10 +1,15 @@
 package com.example.toastlibrary
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 
 object SomethingSpecial {
     fun show(C : Context, M : String){
         Toast.makeText(C,M,Toast.LENGTH_LONG).show()
+    }
+    fun Debugger (context : Context, message : String, tag : String){
+        show(context,message)
+        Log.d(tag,message)
     }
 }
